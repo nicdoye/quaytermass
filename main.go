08bdb0e12828237358b026993cee066feafabb76
Nic Doye <nic.doye@alfresco.com>
2018-06-22 13:35:21 +0100

    Works

--- main.go
+++ main.go
@@ -3,32 +3,27 @@ package main
 
 import (
   "os"
-  "log"
+	"fmt"
 
-	"github.com/nicdoye/quaytermass/client/operations"
   "github.com/go-openapi/strfmt"
-  "github.com/go-openapi/spec"
 
   apiclient "github.com/nicdoye/quaytermass/client"
-  httptransport "github.com/go-openapi/runtime/client"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/nicdoye/quaytermass/client/repository"
 )
 
 func main() {
+  client := apiclient.New(httptransport.New("quay.io", "", nil), strfmt.Default)
 
-  // create the API client
-  client := apiclient.New(httptransport.New("", "", nil), strfmt.Default)
-
-  // make the authenticated request to get all items
   bearerTokenAuth := httptransport.BearerToken(os.Getenv("API_ACCESS_TOKEN"))
-  // basicAuth := httptransport.BasicAuth(os.Getenv("API_USER"), os.Getenv("API_PASSWORD"))
-  // apiKeyQueryAuth := httptransport.APIKeyAuth("apiKey", "query", os.Getenv("API_KEY"))
-  // apiKeyHeaderAuth := httptransport.APIKeyAuth("X-API-TOKEN", "header", os.Getenv("API_KEY"))
-  resp, err := client.Operations.All(operations.AllParams{}, bearerTokenAuth)
-  // resp, err := client.Operations.All(operations.AllParams{}, basicAuth)
-  // resp, err := client.Operations.All(operations.AllParams{}, apiKeyQueryAuth)
-  // resp, err := client.Operations.All(operations.AllParams{}, apiKeyHeaderAuth)
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Printf("%#v\n", resp.Payload)
+
+	params := repository.NewDeleteRepositoryParams()
+	for _, repo := range os.Args[1:] {
+		resp, err := client.Repository.DeleteRepository(params.WithRepository(repo), bearerTokenAuth)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("%v\n",resp)
+		}
+	}
 }
